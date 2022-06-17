2ae78278b96cc9428668d44ff843e0bd5e9a1fd7
질문KoSeungyu < koeul123@gmail.com>
2022-06-17 14:52:24 +0900

    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")

--- src/main/java/com/sparta/clonecoding_8be/security/UserDetailsServiceImpl.java
+++ src/main/java/com/sparta/clonecoding_8be/security/UserDetailsServiceImpl.java
@@ -12,9 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
-
     private final MemberRepository memberRepository;
-
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Member member = memberRepository.findByUsername(username).orElseThrow(
                         () -> new UsernameNotFoundException(username + "이 존재하지 않습니다")
--- src/main/java/com/sparta/clonecoding_8be/util/Timestamped.java
+++ src/main/java/com/sparta/clonecoding_8be/util/Timestamped.java
@@ -1,5 +1,6 @@
 package com.sparta.clonecoding_8be.util;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -15,8 +16,10 @@ import java.time.LocalDateTime;
 public class Timestamped {
 
     @CreatedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime modifiedAt;
 }
