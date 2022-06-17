9b885280f2cc327d7e4a02efbee6f27dd68c378e
waryongc <jip95@naver.com>
2022-06-17 12:21:22 +0900

    클론코딩 시작 #1

---
+++ src/main/java/com/sparta/clonecoding_8be/Clonecoding8BeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.clonecoding_8be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clonecoding8BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Clonecoding8BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/clonecoding_8be/Clonecoding8BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.clonecoding_8be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clonecoding8BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
