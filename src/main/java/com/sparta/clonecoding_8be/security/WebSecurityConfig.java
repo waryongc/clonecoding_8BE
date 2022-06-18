300d16fc66eac917437444cbe98a5bd9c84a8c97
질문KoSeungyu < koeul123@gmail.com>
2022-06-18 13:52:00 +0900

    .antMatchers("/api/posts").permitAll() 추가

--- src/main/java/com/sparta/clonecoding_8be/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/clonecoding_8be/security/WebSecurityConfig.java
@@ -64,6 +64,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/user/**").permitAll()
                 .antMatchers("/oauth/**").permitAll()
+                .antMatchers("/api/posts").permitAll()
                 .antMatchers(HttpMethod.GET,"/api/posts").permitAll()
                 .antMatchers(HttpMethod.GET,"/api/ranks").permitAll()
                 .antMatchers("/auth/**", "/oauth2/**")
